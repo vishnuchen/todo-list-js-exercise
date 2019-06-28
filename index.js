39ffcfdc5f099207f0de6a4291ad84978c2c1011
Vishnu Chennuru <vishnuchen@gmail.com>
2019-06-28 17:32:07 +0000

    working with objects program end

--- index.js
+++ index.js
@@ -1,35 +1,4 @@
-// const taskTitles = [];
-// const taskComplete = [];
-// const taskDescriptions = [];
 
-// function newTask(title, description) {
-//   taskTitles.push(title);
-//   taskDescriptions.push(description);
-//   tasksComplete.push(false);
-// }
-
-// // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-// function completeTask(taskIndex) {
-//   taskComplete[taskIndex] = true;
-// }
-
-// // Print the state of a task to the console in a nice readable way
-// function logTaskState(taskIndex) {
-//   const title = taskTitles[taskIndex];
-//   const complete = taskComplete[taskIndex];
-//   console.log(`${title} has${complete ? " " : " not "}been completed`);
-// }
-
-// // DRIVER CODE BELOW
-
-// newTask("Clean Cat Litter"); // task 0
-// newTask("Do Laundry"); // task 1
-
-// logTaskState(0); // Clean Cat Litter has not been completed
-// completeTask(0);
-// logTaskState(0); // Clean Cat Litter has been completed
-
-// prints out the provided task's details
 function newTask(title, description) {
   const task = {
     title: title,
